556764d300de5f6d3bbc5abdd0702456e1d0c43a
Pramadha Yaramakala <pramadhayaramakala@Pramadhas-MacBook-Pro.local>
2024-04-24 23:30:30 +0530

    progressbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [progress, setProgress] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (progress < 100) {
+        setProgress(progress + 1);
+      } else {
+        clearInterval(interval);
+      }
+    }, 200);
+
+    return () => clearInterval(interval);
+  }, [progress]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h3>Progress Bar</h3>
+      <div className="progress-bar-container">
+        <div
+          className="progress-bar"
+          style={{ width: `${progress}%` }}
         >
-          Learn React
-        </a>
-      </header>
+          <div className="progress-text">{progress}%</div>
+        </div>
+      </div>
+      {progress === 100 ? (
+        <h4 className="loading-text">Completed</h4>
+      ) : (
+        <h4 className="loading-text">Loading...</h4>
+      )}
     </div>
   );
 }
